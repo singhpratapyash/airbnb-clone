fe438e4670c40396da6c02bb38104fd1b1f109ee
singhpratapyash <161168798+singhpratapyash@users.noreply.github.com>
2025-08-14 23:11:10 +0530

    Initialised database and basic CRUD operations

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+async function main(){
+    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+}
+
+main()
+.then(()=>{
+    console.log("db is connected...");
+})
+.catch((err)=>{
+    console.log("error connecting to db.", err);
+})
+
+app.listen(8080, (req,res)=>{
+    console.log("app is listening...");
+});
+
+app.get("/", (req,res)=>{
+    res.send("this is root path...");
+})
