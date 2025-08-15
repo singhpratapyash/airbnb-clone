ce040fb5e9c42e9513872fccc7e0472554fe5a03
singhpratapyash <161168798+singhpratapyash@users.noreply.github.com>
2025-08-15 23:37:30 +0530

    added few ejs templatess with crud operation

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js")
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join( __dirname, "./views"));
+
+app.use(express.urlencoded({extended:true}));
 
 async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
@@ -20,4 +27,18 @@ app.listen(8080, ()=>{
 
 app.get("/", (req,res)=>{
     res.send("this is root path...");
+});
+
+app.get("/listings", async (req,res)=>{
+
+    const allListings = await Listing.find({});
+    res.render("./listings/index.ejs", { allListings } );
+});
+
+app.get("/listings/:id", async (req,res)=>{
+
+    let {id} = req.params;
+    const showListing = await Listing.findById(id);
+    res.render("./listings/show.ejs", {showListing});
+
 })
--- init/index.js
+++ init/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
-const Listing = require("../models/listings.js");
+const Listing = require("../models/listing.js");
 
 async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
