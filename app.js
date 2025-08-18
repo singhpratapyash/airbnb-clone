a692ade211d0c5e052171be90b26cae9fdbcfb5f
singhpratapyash <161168798+singhpratapyash@users.noreply.github.com>
2025-08-19 04:35:32 +0530

    added some stylings

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js")
 const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require('ejs-mate');
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join( __dirname, "./views"));
 
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")))
 
 async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
@@ -36,7 +42,7 @@ app.get("/listings", async (req,res)=>{
 });
 
 app.get("/listings/new", (req,res)=>{
-    res.render("new.ejs");
+    res.render("./listings/new.ejs");
 });
 
 app.get("/listings/:id", async (req,res)=>{
@@ -47,9 +53,30 @@ app.get("/listings/:id", async (req,res)=>{
 
 });
 
-app.post("/listings", (req, res)=>{
+app.post("/listings", async (req, res)=>{
 
-    const data = req.body.listing;
-    console.log(data);
+    const listing = req.body.listing;
+    const newListing = new Listing(listing);
+    await newListing.save();
+    res.redirect("/listings"); 
+});
 
+app.get("/listings/:id/edit", async (req,res)=>{
+    
+    const {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("./listings/edit.ejs", {listing});
+});
+
+app.put("/listings/:id" , async (req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id , req.body.listing, { new:true , runValidators:true });
+    res.redirect("/listings");
+});
+
+app.delete("/listings/:id", async (req,res)=>{
+    const {id} = req.params;
+    const deletedListing = await Listing.findByIdAndDelete(id);
+    console.log("deleted Listing : ", deletedListing);
+    res.redirect("/listings");
 });
